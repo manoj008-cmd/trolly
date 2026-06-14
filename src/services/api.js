1c7a50377d7dc9eff3ccab114409f331e4cdb81a
manoj005-cmd <manoj.23cs066@sode-edu.in>
2026-06-14 21:13:54 +0530

    Add environment variable support

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    "https://toll-management-backend-production.up.railway.app/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default API;
